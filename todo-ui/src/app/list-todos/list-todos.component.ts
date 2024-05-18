0cab92d758bd2e12a86f2001bbb54fef00aedca1
Kannan Rajendran <kannannglabs@gmail.com>
2024-05-19 01:23:17 +0530

    create a todo class

--- todo-ui/src/app/list-todos/list-todos.component.ts
+++ todo-ui/src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ){}
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -7,9 +16,10 @@ import { Component } from '@angular/core';
 })
 export class ListTodosComponent {
   todos = [
-    {id: 1, description: 'Learn to Python'},
-    {id: 2, description: 'Become an expert an angular'},
-    {id: 3, description: 'Planned to GCP certificate'},
+    new Todo(1, 'Learn to Python', false, new Date()),
+    new Todo(1, 'Become an expert an angular', false, new Date()),
+    new Todo(1, 'Planned to GCP certificate', false, new Date()),
+    new Todo(1, 'Learn to singing', false, new Date()),
   ]
   todo = {
     id: 1,
