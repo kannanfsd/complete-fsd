20c19d388153bbd34be1b4c19fd82ad6d4b8deaa
Kannan Rajendran <kannannglabs@gmail.com>
2024-06-04 14:04:12 +0530

    create rest-api for new and modify todo

--- todo-app/src/main/java/com/todo/restapi/controller/TodoResource.java
+++ todo-app/src/main/java/com/todo/restapi/controller/TodoResource.java
@@ -1,14 +1,20 @@
 package com.todo.restapi.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.todo.restapi.bean.Todo;
 import com.todo.restapi.service.TodoHardcodedService;
@@ -39,5 +45,27 @@ public class TodoResource {
 			return ResponseEntity.noContent().build();
 		}
 		return ResponseEntity.notFound().build();
+	}
+	@PutMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Todo> updateTodo(
+			@PathVariable String username,
+			@PathVariable long id, 
+			@RequestBody Todo todo) {
+		Todo todoUpdated = service.save(todo);
+		return new ResponseEntity<Todo>(todoUpdated, HttpStatus.OK);
+	}
+	@PostMapping("/users/{username}/todos")
+	public ResponseEntity<Void> createTodo(
+			@PathVariable String username,			
+			@RequestBody Todo todo) {
+		Todo createdTodo = service.save(todo);
+		// Location
+		// Get current resourse url
+		// {id}
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+								   .path("/{id}")
+								   .buildAndExpand(createdTodo.getId())
+								   .toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
