e2e29185d26f18a00abe2f614a084cd4df69f059
Kannan Rajendran <kannannglabs@gmail.com>
2024-06-07 20:26:54 +0530

    apply basic authentication

--- todo-ui/src/app/service/data/welcome-data.service.ts
+++ todo-ui/src/app/service/data/welcome-data.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 export class HelloWorldBean {
@@ -19,7 +19,21 @@ export class WelcomeDataService {
   }
 
   executeHelloWorldVariable(name: string) {
-    return this.http.get<HelloWorldBean>(`http://localhost:8090/hello-world-bean/path-variable/${name}`);
+    let basicAuth = this.createBasicAuthHeader();
+    console.log('BasicAuth--->'+basicAuth);
+    let headers = new HttpHeaders({
+      Authorization: basicAuth
+    })
+    console.log('Headers--->'+headers);
+    return this.http.get<HelloWorldBean>(`http://localhost:8090/hello-world-bean/path-variable/${name}`, {headers}
+    );
+  }
+
+  createBasicAuthHeader() {
+    let username = 'learning';
+    let password = 'fsd@24';
+    let basicAuthString = 'Basic ' + window.btoa(username + ':' + password);
+    return basicAuthString;
   }
 
 }
